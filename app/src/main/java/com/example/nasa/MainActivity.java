042bd0a99642a873b2e0e386ed024d36897109ab
Amandeep Singh <dhillo27@myumanitoba.ca>
2022-01-27 11:05:59 -0800

    Working App and add README.md file

--- app/src/main/java/com/example/nasa/MainActivity.java
+++ app/src/main/java/com/example/nasa/MainActivity.java
@@ -6,13 +6,27 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.DownloadManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RequestQueue requestQueue;
+    private List<Image> imgList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,10 +38,60 @@ public class MainActivity extends AppCompatActivity {
 
         requestQueue = VolleySingleton.getmInstance(this).getRequestQueue();
 
+        imgList = new ArrayList<>();
+
         fetchData();
     }
 
     private void fetchData(){
-        String url = "https://images-api.nasa.gov/search?q=mars&media_type=image";
+        String url = "https://images-api.nasa.gov/search?q=earth&media_type=image";
+
+        // JSON Object request
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject collection = response.getJSONObject("collection");
+                    JSONArray itemArray = collection.getJSONArray("items");
+
+                    for(int i=0; i<itemArray.length(); i++) {
+                        JSONObject eachItem = itemArray.getJSONObject(i);
+
+                        JSONArray links = eachItem.getJSONArray("links");
+                        JSONArray data = eachItem.getJSONArray("data");
+                        String title =null;
+
+                        for (int j = 0; j < data.length(); j++) {
+                            JSONObject innerData = data.getJSONObject(j);
+                             title = innerData.getString("title");
+
+                        }
+
+                       String imgurl = null;
+                        for (int k = 0; k < links.length(); k++) {
+                            JSONObject link = links.getJSONObject(k);
+                            imgurl = link.getString("href");
+                        }
+
+                        Image item = new Image(imgurl, title);
+                        imgList.add(item);
+                    }
+                    ImageAdapter adapter = new ImageAdapter(MainActivity.this, imgList);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
     }
 }
