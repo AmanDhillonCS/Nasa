fec6625ac13e3f1a6e8d89045906ed17c4d2469a
Amandeep Singh <dhillo27@myumanitoba.ca>
2022-01-26 21:53:05 -0800

    Add VolleySingleton class

--- app/src/main/java/com/example/nasa/MainActivity.java
+++ app/src/main/java/com/example/nasa/MainActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
+
+import com.android.volley.RequestQueue;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-
+    private RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +21,13 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        requestQueue = VolleySingleton.getmInstance(this).getRequestQueue();
+
+        fetchData();
+    }
+
+    private void fetchData(){
+        String url = "https://images-api.nasa.gov/search?q=mars&media_type=image";
     }
 }
